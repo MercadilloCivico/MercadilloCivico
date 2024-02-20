ad55cd9357317464d450b32c545bb3c11c813e41
Máximo <maxivarela95@gmail.com>
2024-02-20 19:26:43 -0300

    feat: :construction: Crea la configuración inicial de rutas en el front, junto a las vistas principales

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css';
-import { Button } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer.jsx';
-import Card from './components/Card/Card.jsx';
+import Home from './views/Home.jsx/Home.jsx';
+import Store from './views/Store/Store.jsx';
+import Contact from './views/Contact/Contact.jsx';
+//import { Button } from '@mui/material';
+//import Card from './components/Card/Card.jsx';
 
 function App() {
   return (
     <div className=''>
-      <p className='text-3xl '>Mercadillo Cívico</p>
-      <Button variant='contained' className=''>
-        Comprar
-      </Button>
-      <Footer />
-      <Card
+      {/**
+       * 
+       <p className='text-3xl '>Mercadillo Cívico</p>
+       <Button variant='contained' className=''>
+         Comprar
+       </Button>
+       <Card
         product={{
           name: 'Pack agua mineral 900 ml. 9 unidades',
           price: '11.999',
@@ -21,7 +26,16 @@ function App() {
           weight: '340g',
           calories: '250',
         }}
-      />
+        />
+      */}
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Store' element={<Store />} />
+        <Route path='/Contact' element={<Contact />} />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
