178a32bb7297688852b74e5c579a5091791a839b
thayrov <thayrovg@gmail.com>
2024-02-21 22:41:51 -0600

    feat: :sparkles: crea componente de input personalizado
    define theme para mui, para aplicar estilos personalizados en componente input

--- front/src/views/Store/Store.jsx
+++ front/src/views/Store/Store.jsx
@@ -1,5 +1,19 @@
+import CustomInput from '../../components/CustomInput/CustomInput';
+import { LuSettings2 } from 'react-icons/lu';
+import { IoSearch } from 'react-icons/io5';
+
 const Store = () => {
-  return <div>Store</div>;
+  return (
+    <>
+      <div>Store</div>
+      <CustomInput
+        placeholder='Busca tu producto...'
+        startIcon={IoSearch}
+        endIcon={LuSettings2}
+        variant='outlined'
+      />
+    </>
+  );
 };
 
 export default Store;
