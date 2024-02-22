8286c5b126f03f6e355b9c71b845b7b1faecbe4a
LeandroNGarcia <leandromaxi2014@gmail.com>
2024-02-22 19:49:12 -0300

    feat: :zap: crud de favoritos hecho
    Crud de favoritos + cambios
    
    closes #34

--- back/src/handlers/Usuario/registerHandler.js
+++ back/src/handlers/Usuario/registerHandler.js
@@ -7,8 +7,17 @@ const registerHandler = async (firstName, lastName, email, password, secondName,
     const repeatEmail = await prisma.usuario.findFirst({
       where: { email },
     });
-    if (repeatEmail)
-      throw new Error('El email ya se encuentra registrado, por favor inicie sesión');
+    if (repeatEmail) {
+      if (repeatEmail.disabled) {
+        await prisma.usuario.update({
+          where: { id: repeatEmail.id },
+          data: { disabled: false },
+        });
+        throw new Error('Su cuenta vinculada al email ha sido reactivada, por favor inicie sesión');
+      } else {
+        throw new Error('El email ya se encuentra registrado, por favor inicie sesión');
+      }
+    }
     const hashPassword = await bcrypt.hash(password, 11);
     let secureUrl;
     if (photo) {
--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -6,6 +6,7 @@ const register = require('../controllers/Usuario/registerController');
 const contraseñaOlvidada = require('../controllers/Usuario/contraseñaOlvidada');
 const recuperarContrasenia = require('../controllers/Usuario/recuperarContraseña');
 const putUsuario = require('../controllers/Usuario/putUsuarioController');
+const FavoriteControllers = require('../controllers/Usuario/favoritesController');
 
 const router = Router();
 
@@ -14,16 +15,19 @@ const router = Router();
 router.get('/auth/google', middleware.authenticateGoogle);
 router.get('/auth/google/callback', middleware.authenticateGoogleCallback);
 router.get('/forgot/password', recuperarContrasenia);
+router.get('/favorites/:id?', FavoriteControllers.get);
 
 // Post
 
 router.post('/login', login);
 router.post('/register', register);
 router.post('/logout', logout);
+router.post('/favorites/:id', FavoriteControllers.addFav);
 
 // Delete
 
 router.delete('/disable/user', deleteUser);
+router.delete('/favorites/:id');
 
 // Put
 
