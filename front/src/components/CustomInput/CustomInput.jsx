700d4a2945e94f3dace8c2ce94bfaf5f0bde605d
thayrov <thayrovg@gmail.com>
2024-02-23 12:37:38 -0600

    fix: :bug: modifica custom input para recibir props de validación

--- front/src/components/CustomInput/CustomInput.jsx
+++ front/src/components/CustomInput/CustomInput.jsx
@@ -7,14 +7,30 @@ const CustomInput = ({
   endIcon: EndIcon,
   variant,
   label,
+  value,
+  name,
+  error = '',
 }) => {
+  const handleChange = (event) => {
+    onChange({
+      target: {
+        name,
+        value: event.target.value,
+      },
+    });
+  };
+
   return (
     <TextField
-      className=' bg-pearl-bush-100 text-tuscany-600 placeholder-hippie-green-300 border-tuscany-600 focus:border-hippie-green-600 hover:border-tuscany-200'
+      className='bg-pearl-bush-100 text-tuscany-600 placeholder-hippie-green-300 border-tuscany-600 focus:border-hippie-green-600 hover:border-tuscany-200'
       variant={variant}
       placeholder={placeholder}
-      onChange={onChange}
+      value={value}
+      onChange={handleChange}
       label={label}
+      name={name}
+      error={!!error}
+      helperText={error}
       InputProps={{
         startAdornment: StartIcon ? (
           <InputAdornment position='start'>
