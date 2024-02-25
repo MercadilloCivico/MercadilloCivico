f995ed796b41ededaa0ce6f088fab3b6e08c9076
sanchez112 <juandavids282005@gmail.com>
2024-02-24 22:02:36 -0500

    build: :sparkles: estilos faltantes de para el login

--- front/src/views/Login/login.jsx
+++ front/src/views/Login/login.jsx
@@ -1,90 +1,82 @@
 import Logo from '../../assets/img/logo-simple.svg';
-import Footer from '../../components/Footer/Footer.jsx';
-// import { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import validacion from './validacion';
-// import inputCustom from '../../components/CustomInput/CustomInput.jsx'
-// import buttoCustom from '../../components/CustomButton/CustomButton.jsx'
+// import Footer from '../../components/Footer/Footer.jsx';
+//  import { useState } from 'react';
+//  import { useNavigate } from 'react-router-dom';
+//  import validacion from './validacion';
+import CustomInput from '../../components/CustomInput/CustomInput';
+import CustomButton from '../../components/CustomButton/CustomButton';
+import { FaGooglePlus } from 'react-icons/fa';
 
 function login() {
-  //   const navigate = useNavigate();
-  //   Estado para los datos del formulario y los errores de validación
-  //   const [register, setRegister] = useState({
-  //     mail: '',
-  //     password: '',
-  //   });
-
-  //   const [errors, setErrors] = useState({});
-
-  // Maneja cambios en los campos del formulario
-  //   const handleInput = (e) => {
-  //     // console.log(e.target.name, e.target.value);
-  //   };
-
-  // Maneja el envío del formulario
-  //   const handleSubmit = (e) => {
-
-  //   };
+  // const navigate = useNavigate();
+  // // Estado para los datos del formulario y los errores de validación
+  // const [register, setRegister] = useState({
+  //   mail: '',
+  //   password: '',
+  // });
 
   return (
-    <div className=''>
-      <img src={Logo} alt='Mercadillo Cívico' className='w-[280px] mt-[20px] mb-[10px]' />
-      <div className='container mx-auto px-4 '>
-        <form className='bg-tuscany-100 rounded-md w-[303px] h-[300px] mb-[30px]  mx-auto '>
-          <p className='text-pearl-bush-950 text-base mb-[20px] mt-[px]'>
-            Ingresa tus datos para iniciar sesión
+    <div>
+      <img src={Logo} alt='Mercadillo Cívico' className='w-[240px] p-1' />
+      <form
+        // onSubmit={}
+        className='bg-tuscany-100 rounded-md max-w-[450px] mx-auto py-8'>
+        <p className='text-pearl-bush-950 text-base'>Ingresa tus datos para iniciar sesión</p>
+        <br />
+
+        <div className='flex flex-col self-center max-w-[400px] min-w-[250px] mx-auto px-8'>
+          <CustomInput
+            label='Correo electrónico'
+            placeholder='Correo electrónico'
+            name='name'
+            type='text'
+            // value={register.name}
+            // onChange={handleInput}
+            // maxLength={15}
+          />
+          {/* <div className='text-pearl-bush-950'>{errors.name}</div> */}
+        </div>
+        <br />
+        <div className='flex flex-col self-center max-w-[400px] min-w-[250px] mx-auto px-8'>
+          <CustomInput
+            label='Contraseña'
+            placeholder='Contraseña'
+            name='lastname'
+            type='text'
+            // value={register.lastname}
+            // onChange={handleInput}
+          />
+          {/* <div className='text-pearl-bush-950'>{errors.lastname}</div> */}
+        </div>
+        <br />
+        <div className='flex justify-center items-center gap-x-16 '>
+          <div className='max-w-[400px]'>
+            <CustomButton type='submit' text='Acceder' />
+          </div>
+          <div className='max-w-[px]'>
+            <CustomButton type='submit' text='Google' icon={FaGooglePlus} />
+          </div>
+        </div>
+        <br />
+        <div className='mt-[50 px]'>
+          <p className='text-pearl-bush-950 text-base'>
+            <a href='/Store' className='text-pearl-bush-950 text-base'>
+              Acceder como invitado
+            </a>{' '}
+            •
+            <a href='/register' className='text-pearl-bush-950 text-base'>
+              {' '}
+              Registrarse
+            </a>
           </p>
-
-          <div className='flex flex-col items-center mb-[15px]'>
-            <div className='flex flex-col items-start'>
-              <label className='text-pearl-bush-950 mb-[2px]'>Correo electrónico</label>
-              <input type='text' name='name' />
-            </div>
-            <div></div>
-          </div>
-          <div className='flex flex-col items-center mb-[35px]'>
-            <div className='flex flex-col items-start'>
-              <label className='text-pearl-bush-950'>Contraseña</label>
-              <input type='text' name='lastname' />
-            </div>
-            <div></div>
-          </div>
-
-          <div className='flex justify-center items-center gap-x-7 '>
-            <div>
-              <button type='submit'>Google</button>
-              {/* <buttoCustom 
-                    text='Acceder'
-                    /> */}
-            </div>
-            <div>
-              <button type='submit'>Google</button>
-            </div>
-          </div>
-          <div className='mt-[20 px]'>
-            <p className='text-pearl-bush-950 text-base'>
-              <a href='/Store' className='text-pearl-bush-950 text-base'>
-                Acceder como invitado
-              </a>{' '}
-              •
-              <a href='' className='text-pearl-bush-950 text-base'>
-                {' '}
-                Registrarse
-              </a>
-            </p>
-            <p className='text-pearl-bush-950 text-base'>
-              <a href='' className='text-pearl-bush-950 text-base'>
-                ¿Olvidaste tu contraseña?
-              </a>
-            </p>
-          </div>
-        </form>
-      </div>
-      <div>
-        <Footer />
-      </div>
+          <p className='text-pearl-bush-950 text-base'>
+            <a href='' className='text-pearl-bush-950 text-base'>
+              ¿Olvidaste tu contraseña?
+            </a>
+          </p>
+        </div>
+      </form>
     </div>
   );
 }
-
 export default login;
