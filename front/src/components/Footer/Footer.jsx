7d553d306d99432cef80e89d6c12d3fd84cfcfa1
Nichub-v <niico.5@live.com.ar>
2024-02-26 19:16:06 -0300

    style: :bug: arreglado footer desalineado

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -7,27 +7,27 @@ export default function Footer() {
         <p className='text-sm space-y-5 my-1'>
           &copy; Mercadillo Cívico® 2024. Todos los derechos reservados.
         </p>
-        <ul className='flex space-x-3 text-xs my-2 justify-center'>
-          <li>
+        <ul className='flex flex-wrap space-x-3 text-xs my-2 justify-center'>
+          <li className='my-1'>
             <a href='#' className='text-pearl-bush-700 hover:text-pearl-bush-900 transition'>
               Términos y condiciones
             </a>
           </li>
-          <li>
+          <li className='my-1'>
             <a href='#' className='text-pearl-bush-700 hover:text-pearl-bush-900 transition'>
               Políticas de privacidad
             </a>
           </li>
-          <li>
+          <li className='my-1'>
             <a href='#' className='text-pearl-bush-700 hover:text-pearl-bush-900 transition'>
               Política de calidad
             </a>
           </li>
         </ul>
 
-        <a href='' className='inline-block my-1 text-pearl-bush-950'>
-          <div className='max-width-10 flex align-middle justify-center'>
-            <span className='leading-4 text-sm line font-semibold'>Síguenos</span>
+        <a href='' className='inline-block my-1 text-pearl-bush-950 '>
+          <div className='ml-4 max-width-10 flex align-middle justify-center'>
+            <span className='leading-3 text-sm font-semibold'>Síguenos</span>
             <TiSocialLinkedin className='w-4 h-4 text-pearl-bush-200 bg-pearl-bush-950 rounded mx-1' />
           </div>
         </a>
