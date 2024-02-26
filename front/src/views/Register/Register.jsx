718d645bd84e7ccdcaf8ea8e6845d9c2ebea6fce
DanielBustos342 <daniel.kpo.342@gmail.com>
2024-02-26 12:04:47 -0300

    fix: :bug: agregue el try cath al registro y el diseño del loading

--- front/src/views/Register/Register.jsx
+++ front/src/views/Register/Register.jsx
@@ -60,7 +60,7 @@ function Register() {
   };
 
   // Maneja el envío del formulario
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     // Verificar si se proporcionó una imagen
@@ -104,8 +104,12 @@ function Register() {
       const isFormValid = requiredFields.every((field) => formData[field].trim() !== '');
 
       if (isFormValid && Object.keys(formErrors).length === 0) {
-        dispatch(register(formData));
-        alert('Formulario enviado');
+        try {
+          await dispatch(register(formData));
+          alert('Formulario enviado');
+        } catch (error) {
+          alert('Error al registrar usuario' + error.message);
+        }
         setFormData({
           imgUrl: null,
           name: '',
