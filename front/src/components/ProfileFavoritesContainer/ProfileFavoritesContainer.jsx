53ac684bc06bc3c604476956aecd9a43ac35d919
Nichub-v <niico.5@live.com.ar>
2024-02-29 23:38:38 -0300

    style: :lipstick: animaciones y estilo mejorado en el profile

--- front/src/components/ProfileFavoritesContainer/ProfileFavoritesContainer.jsx
+++ front/src/components/ProfileFavoritesContainer/ProfileFavoritesContainer.jsx
@@ -1,4 +1,5 @@
 import DropdownCard from '../DropdownCard/DropdownCard.jsx';
+import style from './historyAnim.module.css';
 
 export default function ProfileFavoritesContainer() {
   // Debe recibir un array de objetos por props
@@ -30,7 +31,7 @@ export default function ProfileFavoritesContainer() {
   let i = 0;
 
   return (
-    <div className='max-w-[1280px] mx-auto'>
+    <div className={'max-w-[1280px] p-2 pb-0 flex flex-col items-center ' + style.historyAnim}>
       {arrPrueba.map((item) => {
         return (
           <DropdownCard
@@ -40,6 +41,7 @@ export default function ProfileFavoritesContainer() {
             price={item.price}
             rating={item.rating}
             description={item.description}
+            className='mb-2 max-w-[650px]'
           />
         );
       })}
