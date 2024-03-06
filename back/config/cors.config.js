00b9f49957e4adf59d02a29db3f87682f4f9e8a7
thayrov <thayrovg@gmail.com>
2024-03-05 19:42:20 -0600

    ci: :sparkles: agrega nueva url de front

--- back/config/cors.config.js
+++ back/config/cors.config.js
@@ -2,7 +2,12 @@ const cors = require('cors');
 const { FRONT_URL } = require('./env.config');
 
 const corsConfig = cors({
-  origin: [`${FRONT_URL}`, 'http://localhost:5174', 'https://mer-civ.vercel.app'],
+  origin: [
+    `${FRONT_URL}`,
+    'http://localhost:5174',
+    'https://mer-civ.vercel.app',
+    'https://mer-civ.onrender.com',
+  ],
   methods: 'GET,PUT,PATCH,POST,DELETE',
   credentials: true,
   allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
