98469fc64326c565de916f8ac3f7cfcd38c789fb
Máximo <maxivarela95@gmail.com>
2024-03-13 18:25:27 -0300

    refactor: :recycle: cambia la card de pedidos a la de puntos de venta

--- front/src/components/AllPointsCard/AllPointsCard.jsx
+++ front/src/components/AllPointsCard/AllPointsCard.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
-import { FaClock } from 'react-icons/fa';
-import { FaArrowAltCircleRight } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import { FaArrowAltCircleRight, FaStore } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { fetchSalesPointsAsync } from '../../store/thunks/salesPointThunks';
 
-const AllOrdersCard = () => {
+const AllPointsCard = () => {
+  const { items } = useSelector((state) => state.salesPoint);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    (async () => {
+      await dispatch(fetchSalesPointsAsync());
+    })();
+  }, [dispatch]);
   const [hovered, setHovered] = useState(false);
   const navigate = useNavigate();
 
@@ -11,21 +19,21 @@ const AllOrdersCard = () => {
     <div className='w-[90%] sm:w-[40%] xl:w-[20%] m-2 flex flex-col justify-center items-center bg-pearl-bush-200 rounded-md overflow-hidden shadow-md'>
       <div className='w-full flex justify-between items-center p-2'>
         <div className='flex flex-col text-start text-tuscany-950'>
-          <h4>0</h4>
-          <span className='opacity-70 text-[.8em] md:text-[.9em]'>Pedidos Pendientes</span>
+          <h4>{items?.length}</h4>
+          <span className='opacity-70 text-[.8em] md:text-[.9em]'>Total de Puntos de Venta</span>
         </div>
-        <FaClock className='text-[2.3em] md:text-[2.5em] text-tuscany-500 bg-pearl-bush-100 rounded-lg p-2' />
+        <FaStore className='text-[2.3em] md:text-[2.5em] text-tuscany-500 bg-pearl-bush-100 rounded-lg p-2' />
       </div>
       <div
         className='w-full flex justify-between items-center p-2 bg-pearl-bush-100 cursor-pointer'
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
         onClick={() => {
-          navigate('/admin/orders');
+          navigate('/admin/points');
         }}>
         <span
           className={`text-[.8em] md:text-[.9em] ${hovered ? 'text-tuscany-600' : 'text-tuscany-400'} `}>
-          Ver todos los pedidos
+          Ver todos los puntos...
         </span>
         <FaArrowAltCircleRight
           className={`text-[.9em] md:text-[1em] ${hovered ? 'text-pearl-bush-400' : 'text-pearl-bush-300'}`}
@@ -35,4 +43,4 @@ const AllOrdersCard = () => {
   );
 };
 
-export default AllOrdersCard;
+export default AllPointsCard;
