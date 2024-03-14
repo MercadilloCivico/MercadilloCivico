c4d3d85ae0a49f5c27007b4d2e99c99903ca42c5
thayrov <thayrovg@gmail.com>
2024-03-14 10:45:30 -0600

    fix: :bug: remueve SearchBarFaq component de Faqs.jsx

--- front/src/views/Faqs/Faqs.jsx
+++ front/src/views/Faqs/Faqs.jsx
@@ -1,6 +1,6 @@
 import CustomBreadcrumbs from '../../components/CustomBreadcrumbs/CustomBreadcrumbs';
 import FaqBar from '../../components/FaqBar/FaqBar';
-import SearchBarFaq from '../../components/SearchBarFaq/SearchBarFaq';
+// import SearchBarFaq from '../../components/SearchBarFaq/SearchBarFaq';
 import TopFaqs from '../../components/TopFaqs/TopFaqs';
 import CardsMiniFaqs from '../../components/CardsMiniFaqs/CardsMiniFaqs';
 import CardFaqsLarge from '../../components/CardFaqsLarge/CardFaqsLarge';
@@ -16,9 +16,7 @@ const Faqs = () => {
   return (
     <div className='min-h-[calc(100vh-55px)] flex flex-col'>
       <div>
-        <div className='my-4'>
-          <SearchBarFaq />
-        </div>
+        <div className='my-4'>{/* <SearchBarFaq /> */}</div>
         <div className='mb-4 mx-4 custom-border-b'>
           <CustomBreadcrumbs />
         </div>
